5717492830aaa765a418f1a4444e44d8990220bd
URIEL-GLARIA Ander <Ander.URIEL-GLARIA@cpnv.ch>
2019-12-09 09:34:25 +0100

    snowy day commit

--- Exercices/Tableless Calendar.php
+++ Exercices/Tableless Calendar.php
@@ -1,18 +1,23 @@
-/*
-Projet: Tableless Calendar.php + Tableless Calendar Style.css
-Auteur: Uriel Ander
-Version: 5.0
-Date: 02.12.2019
-*/
 <?php
+/*
+*    Projet: Tableless Calendar.php + Tableless Calendar Style.css
+*   Auteur: Uriel Ander
+*    Version: 5.0
+*    Date: 02.12.2019
+*/
+$day = 1;
+$WeekDays = array("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche");
 echo '<link rel="stylesheet" href="Tableless%20Calendar%20Style.css">';
 echo '<div class="Main">';
-echo '<div>';
-echo '<ul>';
-echo '<li>';
-echo 'yeet';
-echo '</li>';
-echo '</ul>';
-echo '</div>';
-echo '</div>';
+    for ($i = 0; $i < 7; $i++)
+    {
+        for ($j = 0; $j = 5; $j ++)
+        {
+            echo '<div>';
+            echo $day;
+            $day++;
+            echo '</div>';
+        }
+    }
+    echo '</div>';
 ?>
