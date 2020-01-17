a0d0dab982cea8b98ad5319b5b0199ba9fced989
anderuriel <47662467+anderuriel@users.noreply.github.com>
2020-01-17 18:58:49 +0100

    jhnjhgb gjhv-

--- Site/View/login.php
+++ Site/View/login.php
@@ -5,17 +5,17 @@ $title = "Rent a Snow - LogIn";
         <div>
             <h2>Login Form</h2>
 
-            <form action="/action_page.php" method="post">
+            <form action="index.php?action=home" method="post">
                 <div class="imgcontainer">
                     <img src="Content/img_avatar2.png" alt="Avatar" class="avatar">
                 </div>
 
                 <div class="container">
                     <label for="uname"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="uname" required>
+                    <input type="text" placeholder="Enter Username" name="uname" minlength="4" required>
 
                     <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required>
+                    <input type="password" placeholder="Enter Password" name="psw" minlength="8" required>
 
                     <button type="submit">Login</button>
                     <label>
@@ -24,7 +24,7 @@ $title = "Rent a Snow - LogIn";
                 </div>
 
                 <div class="container" style="background-color:#f1f1f1">
-                    <button type="button" class="cancelbtn">Cancel</button>
+                    <button type="reset" class="cancelbtn">Cancel</button>
                     <span class="psw">Forgot <a href="#">password?</a></span>
                 </div>
             </form>
